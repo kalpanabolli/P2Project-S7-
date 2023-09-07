3de48814e8d25a7d2cb4e86679709b8f1047b018
kalpanabolli11@gmail.com <kalpanabolli11@gmail.com>
2023-09-07 13:06:42 +0530

    first commit

---
+++ SomByStudents/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="SomByStudents.MvcApplication" Language="C#" %>
---
+++ SomByStudents/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace SomByStudents.Models
+{
+    public class Student
+    {
+        public int StudentId { get; set; }
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+
+        public int Subject { get; set; }
+
+        public float Marks { get; set; }
+    }
+}
